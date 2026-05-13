346830245e09dbdbfa5b04f872bbdc153b629cc2
Dongubak <x001125@gmail.com>
2026-05-13 11:49:39 +0900

    d
    d

--- algorithm.py
+++ algorithm.py
@@ -1,7 +1,13 @@
 from wafflecarUtil import *
 
-
 def autoDrive_algorithm(original_img, canny_img, H1LD, H1RD, H2LD, H2RD, H3LD, H3RD, V1D, V2D, V3D, V4D, V5D, V6D, V7D, leftLane, rightLane, frontLane, LiDAR, prevComm, status, yolo_detections=None):
+    command = prevComm
+    top_class = ''
+    if yolo_detections:
+        top_class = yolo_detections[0]['class']
+    else:
+        top_class = 'Nothing'
+
     """
     자율주행 알고리즘
 
@@ -26,33 +32,10 @@ def autoDrive_algorithm(original_img, canny_img, H1LD, H1RD, H2LD, H2RD, H3LD, H
     command : 제어 명령 문자열
     status  : 갱신된 상태
     """
-    command = prevComm
-
-    if leftLane and rightLane:
-        if H2LD > H2RD:
-            comamnd = 'H,F3,F3,110,E'
-        elif H2LD < H2RD:
-            command = 'H,F3,F3,190,E'
-        else: # H2LD == H2RD
-            command = 'H,F3,F3,150,E'
-
-    if not rightLane:
-        if 90 < V4D < 120:
-            command = 'H,F3,F3,210,E'
-        elif V4D < 130:
-            command = 'H,F3,F3,250,E'
 
     # ===== DEMO VISUALIZATION START (시연용 - 실습 전 삭제) =====
     if original_img is not None:
         _, w = original_img.shape[:2]
-
-        # 상태 표시 (좌상단)
-        status_labels = {1: 'STATUS 1: LANE FOLLOW', 2: 'STATUS 2: CURVE', 3: 'STATUS 3: LANE FOLLOW', 4: 'STATUS 4: STOP'}
-        status_text = status_labels.get(status, 'STATUS: %d' % status)
-        (tw, th), _ = cv2.getTextSize(status_text, cv2.FONT_HERSHEY_SIMPLEX, 0.65, 2)
-        cv2.rectangle(original_img, (10, 8), (14 + tw, 22 + th), (50, 50, 50), -1)
-        cv2.putText(original_img, status_text, (12, 20 + th - 4),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.65, (255, 255, 255), 2)
 
         # YOLO 감지 결과 표시 (우상단, 감지된 경우에만)
         if yolo_detections:
@@ -64,5 +47,49 @@ def autoDrive_algorithm(original_img, canny_img, H1LD, H1RD, H2LD, H2RD, H3LD, H
             cv2.putText(original_img, det_text, (x0, 20 + dh - 4),
                         cv2.FONT_HERSHEY_SIMPLEX, 0.65, (0, 0, 0), 2)
     # ===== DEMO VISUALIZATION END =====
+
+    type = '직진'
+    # type은 직진, 직진_라이다, 표지판_이용, 신호등, 저속의 값으로 할 수 있어요
+
+    if type == '직진':
+        ## 직진 명령시 사용하세요
+        if leftLane and rightLane:
+            if H2LD > H2RD: ## pass를 지우고 command 값을 넣어주세요
+                pass
+            elif H2LD < H2RD:
+                pass
+            else: # H2LD == H2RD
+                pass
+                
+    elif type == '직진_라이다':
+        ## 직진시 라이다 이용해서 멈출때 사용하세요
+        if 150 <= LiDAR <= 200: # 150mm 와 200mm 사이 거리인 경우 아래 블럭을 실행합니다.
+            # 더 가까운 거리에서 멈춰볼까요?
+            pass
+        else:
+            pass
+        
+
+    elif type == '표지판_이용':
+        ## 직진시 표지판 이용해서 멈출때 사용하세요
+        if top_class != 'Nothing' and top_class == 'STOP_SIGN': # 무엇인가 감지된 경우
+            pass
+        else: # 아무것도 감지 안된 경우
+            pass
+
+    elif type == '신호등':
+        ## 빨간불에 멈출때 사용하세요
+        if top_class != 'Nothing' and top_class == 'red_light':
+            pass
+        else:
+            pass
+
+    elif type == '저속':
+        ## 저속 운용시 사용하세요
+        if top_class != 'Nothing' and top_class == 'number_one':
+            pass
+        else:
+            pass
+
 
     return command, status
